840a5f5d4cd00f76431a4e7de08edecd96f22b1e
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-02 14:01:11 -0300

    feat: create student mongoose model

---
+++ src/domains/student/model.ts
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+import { IStudent } from "../../interfaces/IStudent";
+
+const StudentSchema = new Schema<IStudent>(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+
+    birthDate: Date,
+    phone: String,
+
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      cep: String,
+    },
+
+    monthlyIncome: Number,
+
+    guardian: {
+      fullName: String,
+      relationship: String,
+      cpf: String,
+      email: String,
+      address: String,
+    },
+
+    interestedCourses: {
+      type: [String],
+      validate: [(v: string[]) => v.length <= 3, "Máx 3 cursos"],
+    },
+
+    interestedUniversities: {
+      type: [String],
+      validate: [(v: string[]) => v.length <= 3, "Máx 3 universidades"],
+    },
+
+    role: {
+      type: String,
+      default: "student",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Student = model<IStudent>("Student", StudentSchema);
