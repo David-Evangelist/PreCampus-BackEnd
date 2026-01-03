cd3540d21e554b64acb4cf69a2903fcb652daa62
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-03 00:32:04 -0300

    feat(routes): centralize all sub-routers in a main index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import studentRoutes from "../domains/student/routes";
+import schoolRoutes from "../domains/school/routes";
+import universityRoutes from "../domains/university/routes";
+
+import authRoutes from "./auth.routes";
 
 const routes = Router();
 
 routes.use("/students", studentRoutes);
+routes.use("/schools", schoolRoutes);
+routes.use("/universities", universityRoutes);
+
+routes.use("/auth", authRoutes);
 
 export default routes;
