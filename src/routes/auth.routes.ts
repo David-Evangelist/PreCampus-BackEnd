9e2ace371d6b2297fdc0d4bf95c277a9f9b8b7f0
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-03 00:31:08 -0300

    feat(auth): implement authentication routes and profile protection

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  profile,
+  loginStudent,
+  loginInstitution,
+  logout,
+} from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+router.post("/login/student", loginStudent);
+router.post("/login/institution", loginInstitution);
+router.get("/profile", authMiddleware, profile);
+router.post("/logout", logout);
+
+export default router;
