adc41b425640e3dec2350ff57880c111c2e9329d
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-06 23:22:13 -0300

    fix(auth): ensure cookie is properly cleared on logout

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -102,8 +102,8 @@ export async function profile(req: Request, res: Response) {
 export function logout(req: Request, res: Response) {
   res.clearCookie("token", {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    sameSite: "none",
+    secure: true,
   });
 
   return res.status(200).json({ message: "Logout realizado com sucesso" });
